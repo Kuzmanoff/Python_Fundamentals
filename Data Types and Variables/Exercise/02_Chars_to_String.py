87007abde7a053b87126cdcf6908bb0938bb1840
Bilyan Kuzmanov <145658499+Kuzmanoff@users.noreply.github.com>
2023-09-23 10:00:07 +0300

    Create 02_Chars_to_String.py

---
+++ Data Types and Variables/Exercise/02_Chars_to_String.py
@@ -0,0 +1,7 @@
+# Write a function that receives 3 characters. Concatenate all the characters into one string and print it on the console.
+
+str1 = input()
+str2 = input()
+str3 = input()
+
+print(f'{str1}{str2}{str3}')
