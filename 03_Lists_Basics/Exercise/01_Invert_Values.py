da334927ca64cce30ac98949f402d50c6e207fa2
Bilyan Kuzmanov <145658499+Kuzmanoff@users.noreply.github.com>
2023-10-09 16:25:22 +0300

    Commit

--- 03_Lists_Basics/Exercise/01_Invert_Values.py
+++ 03_Lists_Basics/Exercise/01_Invert_Values.py
@@ -1,4 +1,5 @@
-# Write a program that receives a single string containing positive and negative numbers separated by a single space. 
+# Write a program that receives a single string containing positive and negative numbers 
+# separated by a single space. 
 # Print a list containing the opposite of each number.
 
 numbers_str = input()
--- 03_Lists_Basics/Exercise/02_Multiples_List.py
+++ 03_Lists_Basics/Exercise/02_Multiples_List.py
@@ -1,5 +1,8 @@
 # Write a program that receives two numbers (factor and count). 
-# It should create a list with a length of the given count that contains only integer numbers, which are multiples of the given factor. The numbers should be only positive, and they should be arranged in ascending order, starting from the value of the factor.
+# It should create a list with a length of the given count that contains 
+# only integer numbers, which are multiples of the given factor. 
+# The numbers should be only positive, and they should be arranged in ascending order, 
+# starting from the value of the factor.
 
 n_factor = int(input())
 n_count = int(input())
