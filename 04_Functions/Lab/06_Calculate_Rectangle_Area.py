46323a44e06eca016cd1e55393be20bfb3badffb
Bilyan Kuzmanov <145658499+Kuzmanoff@users.noreply.github.com>
2023-10-10 19:48:03 +0300

    .

--- 04_Functions/Lab/06_Calculate_Rectangle_Area.py
+++ 04_Functions/Lab/06_Calculate_Rectangle_Area.py
@@ -1,1 +1,10 @@
-# Create a function that calculates and returns the area of a rectangle by given width and height. Print the result on the console.
+# Create a function that calculates and returns the area of a rectangle by given width and height. Print the result on the console.def calculate_rectangle_area(width, height):
+    area = width * height
+    return area
+
+width = int(input())
+height = int(input())
+
+area = calculate_rectangle_area(width, height)
+
+print(area)
--- 04_Functions/Lab/07_Rounding.py
+++ 04_Functions/Lab/07_Rounding.py
@@ -1,1 +1,11 @@
 # Write a program that rounds all the given numbers, separated by a single space, and prints the result as a list. Use round().
+numbers_str = input()
+
+
+numbers = numbers_str.split()
+
+
+rounded_numbers = [round(float(num)) for num in numbers]
+
+
+print(rounded_numbers)
